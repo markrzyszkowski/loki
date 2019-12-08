c13ee081ad759605bd4ae90b5ccc333ccbbdb2f1
Marcin Krzyszkowski <marcin.krzyszkowski@gmail.com>
2019-12-08 23:25:49 +0100

    Initial commit

---
+++ loki-server/src/test/java/com/krzyszkowski/loki/server/StartContextTest.java
@@ -0,0 +1,16 @@
+package com.krzyszkowski.loki.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartContextTest {
+
+    @Test
+    public void contextLoads() {
+        // intentionally left empty
+    }
+}
