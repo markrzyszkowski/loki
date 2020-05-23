8b9bd8c57ffd327ea5b940e8dc1b95608f730222
Marcin Krzyszkowski <marcin.krzyszkowski@gmail.com>
2020-05-23 16:46:26 +0200

    Mock logs older than 7 days are now removed on startup

--- loki-agent/src/main/java/com/krzyszkowski/loki/agent/core/internal/EventListeners.java
+++ loki-agent/src/main/java/com/krzyszkowski/loki/agent/core/internal/EventListeners.java
@@ -9,15 +9,37 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.function.BiPredicate;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Component
 public class EventListeners {
+
+    @Value("${logging.file.name}")
+    private String logFile;
 
     @Value("${loki.agent.electron.port}")
     private int electronPort;
 
     @EventListener(ApplicationReadyEvent.class)
     public void applicationReadyEventListener() {
+        try {
+            log.info("Removing old mock log files");
+
+            removeOldLogs();
+        } catch (IOException e) {
+            log.error("Could not remove old mock log files");
+            log.error("Exception: {}", e.toString());
+        }
+
         var callbackUrl = String.format("http://localhost:%d/ready", electronPort);
 
         try {
@@ -30,6 +52,22 @@ public class EventListeners {
         } catch (RestClientException e) {
             log.error("Agent ready callback was not successful");
             log.error("Exception: {}", e.toString());
+        }
+    }
+
+    private void removeOldLogs() throws IOException {
+        var retentionThreshold = Instant.from(ZonedDateTime.now().minusDays(7));
+
+        BiPredicate<Path, BasicFileAttributes> retentionPredicate = (path, attributes) ->
+                path.getFileName().toString().startsWith("mock-")
+                && attributes.lastModifiedTime().compareTo(FileTime.from(retentionThreshold)) < 0;
+
+        var filesToDelete = Files.find(Path.of(logFile).getParent(), 1, retentionPredicate)
+                                 .peek(path -> System.out.println(path.getFileName()))
+                                 .collect(Collectors.toList());
+
+        for (var path : filesToDelete) {
+            Files.delete(path);
         }
     }
 }
